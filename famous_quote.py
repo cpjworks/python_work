0cb6b3072218acb1be93288bef2c50941ec32289
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-07 23:48:29 +0800

    Add files via upload

---
+++ famous_quote.py
@@ -0,0 +1,9 @@
+# 2-5. Famous Quote: Find a quote from a famous person you admire. Print the
+# quote and the name of its author. Your output should look something like the
+# following, including the quotation marks:
+# Albert Einstein once said, “A person who never made a 
+# mistake never tried anything new.”
+
+name = 'tosk:' 
+quote = '"I am Tosk, the hunted. I live to outwit the hunters for another day, to survive, until I die with honor."'
+print(f"\t{name.title()}\n\t{quote}")
