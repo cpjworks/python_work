8c1a6adfeccf686dde22c8cb3195b2aff6317f36
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-08 17:53:49 +0800

    Add files via upload

---
+++ famous_quote_2.py
@@ -0,0 +1,7 @@
+# 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the
+# famous person’s name using a variable called famous_person. Then compose
+# your message and represent it with a new variable called message. Print your message.
+
+famous_person = 'tosk:' 
+message = '"I am Tosk, the hunted. I live to outwit the hunters for another day, to survive, until I die with honor."'
+print(f"\t{famous_person.title()}\n\t{message}")
