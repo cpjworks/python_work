07712b994b474fd1dd69239969727607b5ee543a
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-10 00:00:09 +0800

    Add files via upload

---
+++ favorite_number.py
@@ -0,0 +1,7 @@
+# 2-9. Favorite Number: Use a variable to represent your favorite number. Then,
+# using that variable, create a message that reveals your favorite number. Print
+# that message.
+
+my_number = 13
+message = "Hey there. I thought about it, and my favourite number is "
+print (message,my_number)
