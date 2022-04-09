fe8c1d04b40bc9f0c7f40ec6fad4ac91ce0d2de9
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-10 00:41:53 +0800

    Add files via upload

---
+++ adding_comments.py
@@ -0,0 +1,13 @@
+# 2-10. Adding Comments: Choose two of the programs you’ve written, and
+# add at least one comment to each. If you don’t have anything specific to write
+# because your programs are too simple at this point, just add your name and
+# the current date at the top of each program file. Then write one sentence
+# describing what the program does.
+
+# Chris, 10th April 2022.
+# This program uses some integers, variables, values, an f-string, and then prints them.
+
+total = 3 * 10
+user = "Chris"
+message = f"{user} calculated that the total equals {total}."
+print (message)
