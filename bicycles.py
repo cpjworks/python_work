e181b8b451da5cee90127fed40183f5d249cc2a2
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-10 18:50:59 +0800

    Add files via upload

---
+++ bicycles.py
@@ -0,0 +1,4 @@
+# Testing how Lists work in Python.
+bicycles = ["cannondale", "specialized", "dawes", "peugeot", "scott"]
+message = f"My first bicycle was a {bicycles[-1].title()}."
+print(message)
