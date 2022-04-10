ac15c7151abeb4370865f505b2eacfc037697acf
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-10 21:11:58 +0800

    Add files via upload

---
+++ 03_03_your_own_list.py
@@ -0,0 +1,11 @@
+# 3-3. Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as “I would like to own a
+# Honda motorcycle.”
+
+vehicles = ["all of my bicycles", "Japanese bullet train", "kayaks", "plane", "Honda VFR750"]
+print(f"I really enjoy riding {vehicles[0]}.")
+print(f"I have never been on a {vehicles[1]}.")
+print(f"In Sea Scouts, we would paddle in the {vehicles[2]} every Sunday.")
+print(f"To get to Taiwan from the UK, I need to take a {vehicles[3]}.")
+print(f"The {vehicles[4]} is a motorcycle I would love to go on an adventure with.")
