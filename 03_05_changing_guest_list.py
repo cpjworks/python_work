a921f2462f971a13e4122482e76069d830c69ae5
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-10 22:34:43 +0800

    Add files via upload

---
+++ 03_05_changing_guest_list.py
@@ -0,0 +1,30 @@
+# 3-5. Changing Guest List: You just heard that one of your guests can’t make the
+# dinner, so you need to send out a new set of invitations. You’ll have to think of
+# someone else to invite.
+
+# • Start with your program from Exercise 3-4. Add a print() call at the end
+# of your program stating the name of the guest who can’t make it.
+
+# • Modify your list, replacing the name of the guest who can’t make it with
+# the name of the new person you are inviting.
+
+# • Print a second set of invitation messages, one for each person who is still
+# in your list.
+
+guests = ["malcolm", "tosk", "grandfather", "grandmother"]
+print(f"Helo {guests[0].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[1].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[2].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[3].title()}. You are warmly invited to a dinner gathering.")
+
+guests.remove("tosk")
+print(guests)
+
+guests[1] = "father"
+guests.insert(1, "father")
+
+print(f"Helo {guests[0].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[1].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[2].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[3].title()}. You are warmly invited to a dinner gathering.")
+
