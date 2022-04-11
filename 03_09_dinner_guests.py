268ce2ab7dae3f40f4fe62cd52f2d496d703acc1
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-11 16:15:12 +0800

    Add files via upload

---
+++ 03_09_dinner_guests.py
@@ -0,0 +1,11 @@
+# 3-9. Dinner Guests: Working with one of the programs from Exercises 3-4
+# through 3-7 (page 42), use len() to print a message indicating the number
+# of people you are inviting to dinner.
+
+guests = ["malcolm", "tosk", "grandfather", "grandmother"]
+print(f"Helo {guests[0].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[1].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[2].title()}. You are warmly invited to a dinner gathering.")
+print(f"Helo {guests[3].title()}. You are warmly invited to a dinner gathering.")
+
+print(len(guests))
