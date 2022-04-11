4ffc91d7fffe455287ac9887acb4009464395795
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-11 16:07:46 +0800

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,17 @@
+# Sorting lists in Python
+
+cars = ["bmw", "toyota", "audi", "subaru"]
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
+print("\nHere is the reverse sorted list:")
+cars.reverse()
+print(cars)
+
+len(cars)
