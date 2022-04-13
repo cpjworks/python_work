22c8168522cf3a9ec8a2573a7940cde91994c877
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-13 21:51:20 +0800

    Add files via upload

---
+++ 03_10_every_function.py
@@ -0,0 +1,42 @@
+# 3-10. Every Function: Think of something you could store in a list. For example,
+# you could make a list of mountains, rivers, countries, cities, languages, or anything
+# else you’d like. 
+
+# Write a program that creates a list containing these items
+# and then uses each function introduced in this chapter at least once:
+
+# Functions to use: 7. Use del statement to remove item(s) from list at index location, 8. Remove last item from
+# list using pop, save value, and re-use, 9. Pop specific item using index location, 10. Remove item(s) by value (name), 11.
+# Use sort to change order permanently, 12. Reverse sort, 13. Sorted to rearrange but not perm., 13. Reverse sorted, 14. Reverse
+# method, 14. Use len to find length of list.
+
+# 1. Print indexed items from list
+games = ["metal gear solid", "final fantasy 12", "advance wars", "nier", "talos principle"]
+print(games)
+
+# 2. Print indexed from end of list
+print(games[4].title())
+print(games[-2].title())
+
+# 3. Change value of indexed item
+games[1] = "final fantasy 7"
+print(games)
+
+# 4. Use append to add items to end of list
+games.append("halo")
+print(games)
+
+# 5. Use append to build a new list from scratch
+games = []
+games.append("shenmue")
+games.append("super monkey ball")
+games.append("soul calibur")
+games.append("skies of arcadia")
+games.append("the wind waker")
+print(games)
+
+# 6. Use insert to add a new item to a specific index location
+games.insert(2, "street fighter iii: 3rd strike")
+print(games)
+
+# STILL NEED TO FINISH THIS EXERCISE
