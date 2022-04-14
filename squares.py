e4df74a9aea6841d134e2699c25aa8ea9b4293a0
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-14 21:21:11 +0800

    Add files via upload

---
+++ squares.py
@@ -0,0 +1,8 @@
+# Using range and integers to calculate square numbers / exponents.
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+
+print(squares)
