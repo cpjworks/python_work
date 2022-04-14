d83423c355023676cf71358b12774a602e8409ba
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-14 20:01:19 +0800

    Add files via upload

---
+++ magicians.py
@@ -0,0 +1,8 @@
+# Chapter 4: Working with Loops.
+# Practicing for loops.
+
+magicians = ["alice", "david", "carolina"]
+for magician in magicians:
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+print("Thank you everyone. That was a great magic show.")
