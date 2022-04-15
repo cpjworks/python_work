6bcabfbe21286862956be0f79214dfbc21290340
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-15 16:22:15 +0800

    Update squares.py

--- squares.py
+++ squares.py
@@ -2,7 +2,12 @@
 
 squares = []
 for value in range(1, 11):
-    square = value ** 2
-    squares.append(square)
+    squares.append(value**2)
 
+print(squares)
+
+
+# Same output but using List Comprehensions, which combinine the for loop,
+# creation of new elements, and appending those elements.
+squares = [value**2 for value in range (1, 11)]
 print(squares)
