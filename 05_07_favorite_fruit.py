1ac96abf24271c37cddb3195c5f91389fc68b4db
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-21 19:08:24 +0800

    Add files via upload

---
+++ 05_07_favorite_fruit.py
@@ -0,0 +1,37 @@
+# 5-7. Favorite Fruit: Make a list of your favorite fruits, 
+# and then write a series of independent if statements that 
+# check for certain fruits in your list.
+
+fruits = ["bananas", "grapes", "pineapples", "strawberries", "blueberries"]
+
+if "bananas" in fruits:
+    print("We got 'nanas!")
+if "grapes" in fruits:
+    print("Crispy grapes, right here!")
+if "pineapples" in fruits:
+    print("Someone order spiky and sweet?")
+if "strawberries" in fruits:
+    print("Who knows how to make jam?")
+if "blueberries" in fruits:
+    print("Where is the porridge to add these to?")
+
+
+# • Make a list of your three favorite fruits and call it favorite_fruits.
+
+favourite_fruits = ["bananas", "grapes", "pineapples"]
+
+# • Write five if statements. Each should check whether a certain kind of 
+# fruit is in your list. If the fruit is in your list, 
+# the if block should print a statement, such as You really like bananas!
+
+if "bananas" in favourite_fruits:
+    print("We got 'nanas!")
+if "grapes" in favourite_fruits:
+    print("Crispy grapes, right here!")
+if "pineapples" in favourite_fruits:
+    print("Someone order spiky and sweet?")
+if "strawberries" in favourite_fruits:
+    print("Who knows how to make jam?")
+if "blueberries" in favourite_fruits:
+    print("Where is the porridge to add these to?")
+
