0ccdab1a0dd6e80c327e46511d733fbde62c5722
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-21 17:16:35 +0800

    Add files via upload

---
+++ 05_03_alien_colors_1.py
@@ -0,0 +1,18 @@
+# 5-3. Alien Colors #1: Imagine an alien was just shot down in a game. 
+# Create a variable called alien_color and assign it a value of 'green', 
+# 'yellow', or 'red'.
+
+alien_colour = "red"
+
+# • Write an if statement to test whether the alien’s color is green. 
+
+if alien_colour == "green":
+    print("The alien is still alive. Shoot it down, now!")
+
+# If it is, print a message that the player just earned 5 points.
+# • Write one version of this program that passes the if test and another that
+# fails. (The version that fails will have no output.)
+
+if alien_colour == "red":
+    print("The alien has been eliminated. Congratulations!")
+
