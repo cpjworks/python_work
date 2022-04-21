bccaf616469d02ecb22baf5770499d3554f85e06
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-21 14:23:40 +0800

    Add files via upload

---
+++ 05_01_conditional_tests.py
@@ -0,0 +1,22 @@
+# 5-1. Conditional Tests: Write a series of conditional tests. Print a 
+# statement describing each test and your prediction for the results of each 
+# test. Your code should look something like this:
+
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+# • Look closely at your results, and make sure you understand why each line
+# evaluates to True or False.
+# • Create at least ten tests. Have at least five tests evaluate to True and
+# another five tests evaluate to False.
+
+food = "pizza"
+print("Is food == 'pizza'? I predict True.")
+print(food == "pizza")
+print("\nIs food == 'bread'? I predict False")
+print(food == "bread")
+
+# I understand this so I am not going to spend time writing 8 more tests.
