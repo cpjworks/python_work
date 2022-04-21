f20ec798a0ae574d379ebc97d962a7761234c9e4
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-21 21:32:55 +0800

    Add files via upload

---
+++ 05_13_your_ideas.py
@@ -0,0 +1,18 @@
+# 5-13. Your Ideas: At this point, you’re a more capable programmer than you
+# were when you started this book. Now that you have a better sense of how
+# real-world situations are modeled in programs, you might be thinking of 
+# some problems you could solve with your own programs.
+
+# Record any new ideas you have about problems you might want to solve as 
+# your programming skills continue to improve. 
+
+# Consider games you might want to write, data sets you 
+# might want to explore, and web applications you’d like to create.
+
+# 1. Rez-style on-rails shooting game.
+# 2. Lemming-style game with cute Nier robots.
+# 3. Puyo-Puyo-style falling pieces games.
+
+# 1. Weather data; in particular, extreme weather events.
+
+# 1. Applications: Audio sharing website.
