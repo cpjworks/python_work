c69579c9cb72d5e3bfd07104d46ad29c90dfe450
Chris <91273423+cpjworks@users.noreply.github.com>
2022-04-21 16:55:41 +0800

    Update toppings.py

--- toppings.py
+++ toppings.py
@@ -1,6 +1,14 @@
 # Checking for inequalities in "if" statements.
+# Using mutiple if statements if several tests might be True.
+# Do not use if-elif-else chain in this case.
 
-requested_toppings = "mushrooms"
+requested_toppings = ["mushrooms", "extra cheese"]
 
-if requested_toppings != "anchovies":
-    print("Hold the anchovies!")
+if "mushrooms" in requested_toppings:
+    print("Adding mushrooms.")
+if "pepperoni" in requested_toppings:
+    print("Adding pepperoni.")
+if "extra cheese" in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
